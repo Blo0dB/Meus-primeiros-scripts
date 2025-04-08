ec03e3ba6bde37f76516c9a45b64b15abce66c05
Blo0dB <ygorlop38@gmail.com>
2025-04-07 23:35:46 -0300

    primeiro

---
+++ primeiroscriptquefiz.py
@@ -0,0 +1,7 @@
+a = input ('qual seu nome ')
+b = input ('qual sua idade ')
+c = input ('qual seu peso ')
+print ('eu ja sabia que era '+a,',alguem com '+b,'anos','pesar '+c,'kg c ta gordo em')
+
+
+
